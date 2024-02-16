95d901824e391d854a0a21be2d665a024aa160f0
Alecto <fomenkoandrey1978@gmail.com>
2024-02-16 10:11:35 +0200

    додано тест 4

--- src/main.js
+++ src/main.js
@@ -61,4 +61,41 @@ class SkillsManager {
 // console.log(skillsManager.addSkill('CSS'))
 // console.log(skillsManager.getAllSkills())
 
-export { doubleArrayElements, sumArray, SkillsManager }
+
+/*
+ * #4
+ * Задача: Калькулятор дат.
+ * Завдання: Створити модуль на JavaScript, який імплементує функцію-конструктор DateCalculator для створення об'єктів, здатних керувати датами. Калькулятор дат має надавати такі можливості:
+ * Додавання днів: Метод addDays приймає кількість днів як аргумент і додає цю кількість до поточної дати об'єкта.
+ * Віднімання днів: Метод subtractDays приймає кількість днів як аргумент і віднімає цю кількість від поточної дати об'єкта.
+ * Отримання результату: Метод getResult повертає поточну дату об'єкта у форматі "YYYY-MM-DD".
+ *
+ * Критерії перевірки:
+ * В модулі має бути визначена функція-конструктор DateCalculator, яка ініціалізує об'єкт з початковою датою.
+ * Мають бути реалізовані та доступні методи addDays, subtractDays, та getResult для екземплярів DateCalculator.
+ * Об'єкти DateCalculator мають створюватися за допомогою ключового слова new і використання функції-конструктора.
+ */
+
+function DateCalculator(initialDate) {
+  this.addDays = function(days) {
+    // code here
+  }
+
+  this.subtractDays = function(days) {
+    // code here
+  }
+
+  this.getResult = function() {
+    // code here
+  }
+}
+
+// Демонстрація використання
+// const dateCalculator = new DateCalculator('2023-01-01')
+// dateCalculator.addDays(5)
+// console.log(dateCalculator.getResult()) // Виводить нову дату після додавання днів
+//
+// dateCalculator.subtractDays(3)
+// console.log(dateCalculator.getResult()) // Виводить нову дату після віднімання днів
+
+export { doubleArrayElements, sumArray, SkillsManager, DateCalculator }
